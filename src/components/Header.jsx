a50a8d553e011cd234cc17bc2090df1542f64fb8
rdanov <rdanov80@gmail.com>
2022-07-29 16:03:33 +0300

    create project Shop

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return <nav className="blue lighten-1">
+                <div className="nav-wrapper">
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                   {/*  <li><a href="sass.html">Sass</a></li>
+                    <li><a href="badges.html">Components</a></li>
+                    <li><a href="collapsible.html">JavaScript</a></li> */}
+                </ul>
+                </div>
+        </nav>
+}
+
+export default Header;
